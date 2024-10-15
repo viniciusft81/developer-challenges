9dccef29feca732b2fb24620000ecf5112133416
Vinicius Tonini <viniciusft81@gmail.com>
2024-10-15 15:49:09 -0300

    config: add prisma and generate machine table

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('hello')
+  async getHello() {
+    const machine = await this.prisma.machine.create({
+      data: {
+        idMachine: "maq1",
+        name: 'My Machine',
+        type: 'Fan',
+      }
+    })
+    return {
+      machine,
+    };
   }
 }

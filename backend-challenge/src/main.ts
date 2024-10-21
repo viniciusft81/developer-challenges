a9c18ecd49d33be39105cf6434f551b9da0bb8ec
Vinicius Tonini <viniciusft81@gmail.com>
2024-10-21 11:02:53 -0300

    fix: config cors for integration with frontend

--- backend-challenge/src/main.ts
+++ backend-challenge/src/main.ts
@@ -15,6 +15,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   
   await app.listen(3000);
 }
